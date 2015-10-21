671c76ed2e63246ea102672fe22773b960146c87
Alon Motro <amotro@gannett.com>
2015-10-21 17:22:23 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by amotro on 10/20/15.
+ */
+var restify = require('restify');
+var http = require('http');
+var morelikethis = require('./lib/morelikethis.js');
+
+var ip_address = '127.0.0.1';
+var port = '8080';
+
+var server = restify.createServer({
+    name : "MoreLikeThis"
+})
+
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.CORS());
+
+morelikethis.moreLikeThis(server);
+
+server.listen(port, ip_address, function(){
+    console.log('%s listening at %s ', server.name, server.url);
+});
+
