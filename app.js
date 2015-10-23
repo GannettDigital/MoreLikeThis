fac2d1bb551c8c7e3b8041753bc2ba6014596383
Alon Motro <amotro@gannett.com>
2015-10-23 17:30:16 -0400

    added async.each

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
  * Created by amotro on 10/20/15.
  */
 var restify = require('restify');
+var http = require('http');
 var morelikethis = require('./lib/morelikethis.js');
 
 var ip_address = '127.0.0.1';
@@ -10,6 +11,8 @@ var port = '8983';
 var server = restify.createServer({
     name : "MoreLikeThis"
 })
+
+http.globalAgent.maxSockets = 1000;
 
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
